9c59fe4becec36d907c8b69a4d54869fb158b827
yunsseong <aster20@hanyang.ac.kr>
2024-07-05 00:05:01 +0900

    [FEAT] 게임 종료 조건 달성 종료 메세지 기능 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,9 +1,11 @@
 package baseball;
 
+import java.util.List;
+
 public class Controller {
     private static Controller controller;
-    private Model model;
-    private View view;
+    private final Model model;
+    private final View view;
 
     private Controller(Model model, View view){
         this.model = model;
@@ -25,12 +27,30 @@ public class Controller {
             throw new IllegalArgumentException();
     }
 
+    public boolean statusCheck(Integer strike){
+        return strike == 3;
+    }
+
+    public boolean userDecisionCheck(){
+        String userDecision = view.input("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", true);
+        return userDecision.equals("1");
+    }
+
     public void play(){
-        String userInput = view.input("숫자를 입력해주세요 : ");
-        try{
+        String userInput = view.input("숫자를 입력해주세요 : ", false);
+        try {
             validate(userInput);
-        } catch (Exception e){
+        } catch (Exception e) {
             System.exit(0);
         }
+
+        List<Integer> result = model.generateHint(userInput);
+        view.printResult(result);
+
+        boolean isGameEnd = statusCheck(result.get(1));
+        if(isGameEnd) {
+            view.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        play();
     }
 }
