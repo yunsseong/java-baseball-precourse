afe7a5e049d77b02a3fd3d2535116b2b8fb38ff0
yunsseong <aster20@hanyang.ac.kr>
2024-07-04 14:14:02 +0900

    [FIX] 요구사항에 맞춰 랜덤 메서드 변경

--- src/main/java/baseball/Model.java
+++ src/main/java/baseball/Model.java
@@ -5,8 +5,16 @@ import java.util.List;
 
 public class Model {
     private List<Integer> opponentNumber;
+    int NUMBER_SIZE = 3;
 
     public void generateNumber(){
-        opponentNumber = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        Integer randomInteger = 0;
+        while(true){
+            randomInteger = Randoms.pickNumberInRange(1, 9);
+            if(!opponentNumber.contains(randomInteger))
+                opponentNumber.add(randomInteger);
+            if(opponentNumber.size() == NUMBER_SIZE)
+                break;
+        }
     }
 }
