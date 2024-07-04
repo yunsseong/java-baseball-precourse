4132ad790f6d0145df8f8f75945293ae42d3f0f2
yunsseong <aster20@hanyang.ac.kr>
2024-07-04 16:13:38 +0900

    [FEAT] 상대방 수와 게임 플레이어 수 비교 결과 저장 기능 구현

--- src/main/java/baseball/Model.java
+++ src/main/java/baseball/Model.java
@@ -1,20 +1,40 @@
 package baseball;
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Model {
-    private List<Integer> opponentNumber;
+    private List<String> opponentNumber;
     int NUMBER_SIZE = 3;
 
     public void generateNumber(){
-        Integer randomInteger = 0;
+        int randomInteger;
         while(true){
             randomInteger = Randoms.pickNumberInRange(1, 9);
-            if(!opponentNumber.contains(randomInteger))
-                opponentNumber.add(randomInteger);
+            if(!opponentNumber.contains(Integer.toString(randomInteger)))
+                opponentNumber.add(Integer.toString(randomInteger));
             if(opponentNumber.size() == NUMBER_SIZE)
                 break;
         }
+    }
+
+    public List<Integer> generateHint(String numbers){
+        Integer strike = 0;
+        Integer ball = 0;
+
+        for(int i = 0; i < numbers.length(); i++){
+            String number = Character.toString(numbers.charAt(i));
+            if(number.equals(opponentNumber.get(i))){
+                strike++;
+            } else if (opponentNumber.contains(number)) {
+                ball++;
+            }
+        }
+
+        return new ArrayList<>(Arrays.asList(ball, strike)) ;
+
+
     }
 }
