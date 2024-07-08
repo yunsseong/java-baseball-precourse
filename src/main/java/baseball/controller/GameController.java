a5bcfb7b0c7eb4fd5f7d30ba93425d119a0bd8ed
yunsseong <aster20@hanyang.ac.kr>
2024-07-08 16:30:32 +0900

    feat(Application, ComputerNumGenerator, GameController): computer number generate feature implement

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,18 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumGenerator;
+import baseball.view.Message;
+
 public class GameController {
+	private final ComputerNumGenerator computerNumGenerator;
+	private final Message message;
+
+	public GameController() {
+		this.computerNumGenerator = new ComputerNumGenerator();
+		this.message = new Message();
+	}
+
+	public void init() {
+		computerNumGenerator.generateComputerNum();
+	}
 }
--- src/main/java/baseball/model/ComputerNumGenerator.java
+++ src/main/java/baseball/model/ComputerNumGenerator.java
@@ -1,4 +1,18 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class ComputerNumGenerator {
+	private final List<Integer> numberList = new ArrayList<>();
+
+	public void generateComputerNum() {
+		do {
+			Integer randomNum = Randoms.pickNumberInRange(1, 9);
+			if (!numberList.contains(randomNum))
+				numberList.add(randomNum);
+		} while (numberList.size() != 3);
+	}
 }
