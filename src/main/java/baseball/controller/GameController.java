459feaf6911361f7ebd34af3349048d6fc498a86
yunsseong <aster20@hanyang.ac.kr>
2024-07-11 00:08:48 +0900

    feat(GameController): print game clear message implement

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -27,9 +27,10 @@ public class GameController {
 			compareResultList = computerNum.compareNum(userNum);
 			message.printResult(compareResultList);
 		} while (isGameEnd(compareResultList));
+		message.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 
-	public boolean isGameEnd(List<Integer> result){
+	public boolean isGameEnd(List<Integer> result) {
 		return !result.get(1).equals(3);
 	}
 }
